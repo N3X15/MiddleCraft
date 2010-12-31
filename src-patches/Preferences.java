a532bf78a61acfcdaf5644c19980b4fa2228ad1a
Joshua 'Skrylar' Cearley <joshua.cearley@gmail.com>
2010-12-30 20:10:19 -0600

    Changed PatchRename to follow single parameter annotation standard.

--- src-patches/Preferences.java
+++ src-patches/Preferences.java
@@ -6,36 +6,36 @@ import java.io.*;
 import java.lang.annotation.*;
 import java.util.logging.*;
 
-@PatchRename(targetName = "dt")
+@PatchRename("dt")
 public class Preferences {
 	public static Logger l = Logger.getLogger("Minecraft");
 	protected static DataJack dj = DataJack.getGlobal();
 	
-	@PatchRename(targetName = "dt")
+	@PatchRename("dt")
 	public Preferences(File _file) {
 		l.info("server.properties diverted through DataJack");
 	}
 	
-	@PatchRename(targetName = "a")
+	@PatchRename("a")
 	public void regenerateProperties() {saveProperties();}
 	
-	@PatchRename(targetName = "b")
+	@PatchRename("b")
 	public void saveProperties() {
 		l.info("Pretending to save properties.");
 		// XXX: Ask datajack to flush changes to disk
 	}
 	
-	@PatchRename(targetName = "a")
+	@PatchRename("a")
 	public String getString(String _name, String _default) {
 		return dj.getString(_name, _default);
 	}
 	
-	@PatchRename(targetName = "a")
+	@PatchRename("a")
 	public int getInteger(String _name, int _default) {
 		return dj.getInteger(_name, _default);
 	}
 	
-	@PatchRename(targetName = "a")
+	@PatchRename("a")
 	public boolean getBoolean(String _name, boolean _default) {
 		return dj.getBoolean(_name, _default);
 	}
--- src/org/middlecraft/patcher/reflect/PatchRename.java
+++ src/org/middlecraft/patcher/reflect/PatchRename.java
@@ -2,6 +2,20 @@ package org.middlecraft.patcher.reflect;
 
 import java.lang.annotation.*;
 
+/**
+ * Indicates an item should be renamed upon patch compilation.
+ *
+ * When replacing classes in their entirety it may be desirable to assign
+ * readable names to the patch methods, but they must be compiled to their
+ * obfuscated forms to be useful at runtime.  This annotation tells the
+ * patch compiler to rename a class, method or field so it takes on the
+ * obfuscated name while leaving the source code with the more readable
+ * names.
+ *
+ * @author Joshua 'Skrylar' Cearley
+ */
+@Documented
 public @interface PatchRename {
-	String targetName();
+	/** The name the affected class, member or field will take at runtime. */
+	String value();
 }
