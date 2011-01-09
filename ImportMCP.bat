9814f77c130a19e60b4af258b10ac14f0b6319a0
N3X15 <nexisentertainment@gmail.com>
2011-01-09 13:30:17 -0800

    MCP 2.6 Test 5

--- ImportMCP.bat
+++ ImportMCP.bat
@@ -1,7 +1,6 @@
 @echo off
-echo Importing MCP 2.6 Test 4
+echo Importing MCP 2.6 Test 5
 python lib/scripts/ImportMCP.py
 copy mappings.yml data\server\1.1_02\
-ant full-setup
-java -Xmx1024M -Xms1024M -jar staged/middlecraft.jar GetServerInterfaces
+ant full-setup && java -Xmx1024M -Xms1024M -jar staged/middlecraft.jar GetServerInterfaces
 pause
