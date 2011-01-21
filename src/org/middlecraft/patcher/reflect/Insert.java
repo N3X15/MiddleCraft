208135d5b8c082c7280d208c1bface329617adb6
N3X15 <nexisentertainment@gmail.com>
2011-01-21 02:38:09 -0800

    Copyright crap updated.

--- src/org/middlecraft/patcher/reflect/Insert.java
+++ src/org/middlecraft/patcher/reflect/Insert.java
@@ -1,6 +1,6 @@
 package org.middlecraft.patcher.reflect;
 /**
- * Copyright (c) 2010, MiddleCraft Contributors
+ * Copyright (c) 2010-2011, MiddleCraft Contributors
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
